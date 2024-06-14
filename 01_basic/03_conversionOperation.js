0edeb866a6930d095c75d44a3c2c572d5ee11292
decca1011 <deepakpatil101197@gmail.com>
2024-06-14 16:26:35 +0530

    comparison

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -12,26 +12,26 @@ let valueInNumber = Number(score)
 // "33abc" => NaN
 //  true => 1; false => 0
 
-let isLoggedIn = "hitesh"
+let isLoggedIn = "www"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-//  console.log(booleanIsLoggedIn);
+ console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
 // "hitesh" => true
 
-let someNumber = 33
+let someNumber = 333
 
 let stringNumber = String(someNumber)
-// console.log(stringNumber);
-// console.log(typeof stringNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
 
 // *********************** Operations ***********************
 
 let value = 3
 let negValue = -value
-// console.log(negValue);
+console.log(negValue);
 
 // console.log(2+2);
 // console.log(2-2);
@@ -54,7 +54,7 @@ let str3 = str1 + str2
 // console.log( (3 + 4) * 5 % 3);
 
 // console.log(+true);
-// console.log(+"");
+console.log(+"");
 
 let num1, num2, num3
 
