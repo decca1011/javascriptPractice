b1b9949150378d36d69d8fe9940ba7e7a24837da
decca1011 <deepakpatil101197@gmail.com>
2024-06-20 15:12:17 +0530

    iterator

---
+++ 05_iterators/01_forLoop.js
@@ -0,0 +1,37 @@
+// for
+// for (let index = 0; index < array.length; index++) {
+//    const element = array[index];
+   
+// };
+
+// for( let i =0; i <= 10 ; i++ ) {
+//    console.log(`outer loop value: ${i}`)
+//    for( let j =0; j <= 10 ; j++ ){
+//       //console.log(`inner loop values ${j} and ineer loop ${i}`);
+//      console.log(i + ' * '+ j + ' = ' + i*j);
+
+//    }
+// }
+
+// continue and break
+
+// for (let index = 0; index < 20 ; index++) {
+//    if(index == 5){
+//       console.log(`detected ${index}`)
+// break;
+//    }
+//   console.log(`value of i is ${index}`)
+// }
+
+for (let index = 0; index <= 20 ; index++) {
+   if(index == 5){
+      console.log(`detected ${index}`)
+      continue;
+   }
+  console.log(`value of i is ${index}`)
+}
+
+
+
+
+
---
+++ 05_iterators/02_while.js
@@ -0,0 +1,35 @@
+// while (condition) {
+   
+// }
+
+// do {
+   
+// } while (condition);
+
+
+// let index = 0;
+// while(index <=20) {
+//    console.log(`value of index is ${index}`)
+//    index = index +2
+// }
+
+
+let myArray = ['flash', 'batman', 'superman', 'hulka']
+
+let arr = 0
+// while(arr < myArray.length){
+//    console.log(`value of ${myArray[arr]}`);
+//    arr ++;
+// }
+
+let score = 1;
+
+ do {
+   console.log(`Score is ${score}`)
+   score ++
+   
+ } while (score <= 10);
+  
+  
+
+
